73c1d9d444abe5613b9a045fe03657a59736eb3c
Mary Mederos <emau757@gmail.com>
2016-10-25 09:53:20 -0400

    Revised Sinatra ToDo Code per Instructor Review

--- Week2/Day6/EX_SinatraTODO/lib/task.rb
+++ Week2/Day6/EX_SinatraTODO/lib/task.rb
@@ -1,17 +1,23 @@
 class Task
     require 'time'
-    attr_reader :content, :id, :created_at, :updated_at
+    attr_reader :content, :id, :created_at
     @@current_id = 1
-    def initialize(content, created_at, updated_at)
+    def initialize(content)
         @content = content
         @id = @@current_id
         @@current_id += 1
-        @completed = false
+        @completed = false 		# => By default, every task is incomplete.
         @created_at = Time.now
         @updated_at = nil
     end
     def complete?				# => Returns False.
     	@completed
+    	# -- Layman Solution: -- #
+    	# if @complete == true
+    	# 	true
+    	# else
+    	# 	false
+    	# end
     end
    	def complete!				# => Returns True.
    		@completed = true
@@ -19,8 +25,9 @@ class Task
     def make_incomplete!		# => Opposite of complete! class. (Returns False.)
     	@completed = false
     end
-    def update_content!
+    def update_content!(new_content)
+    	@content = new_content
     	@updated_at = Time.now				# => How to test?
-    	@content.replace("Walk the dog")	# => Shouldn't the string replacement be dynamic?
+    	# @content.replace("Walk the dog")	# => Shouldn't the string replacement be dynamic?
     end
 end
--- Week2/Day6/EX_SinatraTODO/lib/todolist.rb
+++ Week2/Day6/EX_SinatraTODO/lib/todolist.rb
@@ -6,12 +6,16 @@ class TodoList
     def add_task(new_task)
     	@tasks.push(new_task)
     end
-    def delete_task
-    	@tasks.delete_if do |remove_task|
-    		remove_task 
+    def delete_task(task_id)
+    	@tasks.delete_if do |the_task|
+    		the_task.id == task_id
     	end
     end
-    def find_task_by_id
-
+    def find_task_by_id(task_id)
+    	@tasks.find do |the_task|
+    		the_task.id == task_id
+    		# => Why does this return nil if nothing found to match by default??
+    		# => Nvm, default function of .find method, lol!
+    	end
     end
 end
