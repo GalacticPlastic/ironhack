ac9cadd61c46239c67bda06d0e0723c0202ddb5b
Mary Mederos <emau757@gmail.com>
2016-10-25 09:52:23 -0400

    Moved Unit Tests for ToDoList Class to Separate Spec File.

--- Week2/Day6/EX_SinatraTODO/spec/task_spec.rb
+++ Week2/Day6/EX_SinatraTODO/spec/task_spec.rb
@@ -4,35 +4,31 @@ RSpec.describe Task do
 	# 	Task.new
 	# end
   	describe "#complete?" do
-  		task1 = Task.new("Buy the milk", Time.now, nil)
   		it "Returns the task as false because it is not complete" do
+	  		task1 = Task.new("Come in with the milk")
 	  		expect(task1.complete?).to eq(false)
 	    end
 	end
 	describe "#complete!" do
-  		task1 = Task.new("Buy the milk", Time.now, nil)
   		it "Returns the task as true because it is complete" do
-	  		expect(task1.complete!).to eq(true)
+	  		task2 = Task.new("Come in with the milk")
+	  		task.complete!								# => Test side effect, not method directly.
+	  		expect(task2.complete?).to eq(true)
 	    end
 	end
 	describe "#make_incomplete!" do
-  		task1 = Task.new("Buy the milk", Time.now, nil)
   		it "Returns the task as false because it was changed to incomplete" do
-	  		expect(task1.make_incomplete!).to eq(false)
+	  		task3 = Task.new("Come in with the milk")
+	  		task.complete!
+	  		task.make_incomplete!
+	  		expect(task3.complete?).to eq(false)
 	    end
 	end
 	describe "#update_content!" do
- 		task2 = Task.new("Buy the milk", Time.now, Time.now)
 		it "Replaces the task content" do
-	  		expect(task2.update_content!).to eq("Walk the dog")		# => Do I need ("") ?
-		end
-	end
-end
-RSpec.describe TodoList do
-	describe "#add_task" do
-		new_task = TodoList.new
-		it "" do
-			expect(new_task.add_task).to eq()
+	  		task4 = Task.new("Come in with the milk")
+	  		task.update_content!("Walk the dog")
+	  		expect(task.content).to eq("Walk the dog")		# => Do I need ("") ?
 		end
 	end
 end
