a4825a37b9094e8dee96f7e07b24f925ed625ad1
Mary Mederos <emau757@gmail.com>
2016-10-26 15:13:53 -0400

    Validated Insecure Password Using Conditional Regular Expressions

--- Week2/Day8/PP_InsecurePasswords/lib/password_checker.rb
+++ Week2/Day8/PP_InsecurePasswords/lib/password_checker.rb
@@ -1,28 +1,30 @@
 class PasswordChecker
 	def check_password(email, password)
-		if ___ && ____ != email
+		user_domain = email.split(/[@.]/)
+		password_array = password.chars  				# => .to_a
+		uppercase = password =~ (/[[:upper:]]/)
+		lowercase = password =~ (/[[:lower:]]/)
+		numbers = password =~ (/[[:digit:]]/)
+		symbol = password =~ (/[[:punct:]]/)			# => (/^[[::]]$/)
+		if password.length < 7
+			false
+		elsif password.include?(user_domain[0])
+			false
+		elsif password.include?(user_domain[1])
+			false
+		elsif uppercase == nil
+			false
+		elsif lowercase == nil
+			false
+		elsif numbers == nil
+			false
+		elsif symbol == nil
+			false
+		else
 			true
-		elsif
-			false
-		elsif
-			false
-		elsif
-			false
-		elsif
-			false
-		elsif
-			false
-		elsif
-			false
 		end
 	end
 end
-
-password = gets.chomp
-password.length
-
-.to_char
-
 # => Longer than 7 characters
 # => Contain at least 1 letter, 1 number and 1 symbol (e.g. "#", "%"; etc.)
 # => Contain at least 1 uppercase and 1 lowercase letter
--- Week2/Day8/PP_InsecurePasswords/spec/password_checker_spec.rb
+++ Week2/Day8/PP_InsecurePasswords/spec/password_checker_spec.rb
@@ -1,50 +1,50 @@
 require_relative("../lib/password_checker.rb")
 RSpec.describe PasswordChecker do 
-	describe "#check_password"
+	describe "#check_password" do
 		it "returns true for valid passwords" do
 			validate = PasswordChecker.new
 			expect(validate.check_password("nizar@example.com", "aB76*!cz#3")).to eq (true)
 		end
 		it "returns false for passwords shorter than 8 characters" do
-			# => Ex: "ab7*", "l0V&e"
 			validate = PasswordChecker.new
-			expect(validate.check_password("", "")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "aB7*")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "uP8&$")).to eq (false)
 		end
 		it "returns false for passwords without letters" do
-			# => Ex: "123456789", "0!0.1*0@999"
 			validate = PasswordChecker.new
-			expect(validate.check_password("", "")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "5876*%62#3")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "388&$920")).to eq (false)
 		end
 		it "returns false for passwords without numbers" do
-			# => Ex: "z*s*R*cks!", "uPB&$KZl"
 			validate = PasswordChecker.new
-			expect(validate.check_password("", "")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "aBphg*%cz#f")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "uPB&$KZl")).to eq (false)
 		end
 		it "returns false for passwords without symbols" do
-			# => Ex: ""
 			validate = PasswordChecker.new
-			expect(validate.check_password("", "")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "aB76vkcz33")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "uP869O20")).to eq (false)
 		end
 		it "returns false for passwords without uppercase letters" do
-			# => Ex: ""
 			validate = PasswordChecker.new
-			expect(validate.check_password("", "")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "ab76*%cz#3")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "up8&$o20")).to eq (false)
 		end
 		it "returns false for passwords without lowercase letters" do
-			# => Ex: ""
 			validate = PasswordChecker.new
-			expect(validate.check_password("", "")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "AB76*%CZ#3")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "UP8&$O20")).to eq (false)
 		end
 		# => BONUS!
 		it "returns false for passwords with the email username" do
-			# => Ex: ""
 			validate = PasswordChecker.new
-			expect(validate.check_password("", "")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "aB7nizar6*%cz#3")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "unizarP8&$O20")).to eq (false)
 		end
 		it "returns false for passwords with the email domain" do
-			# => Ex: ""
 			validate = PasswordChecker.new
-			expect(validate.check_password("", "")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "aB76*example%cz#3")).to eq (false)
+			expect(validate.check_password("nizar@example.com", "uP8&example$O20")).to eq (false)
 		end
 	end
 end
