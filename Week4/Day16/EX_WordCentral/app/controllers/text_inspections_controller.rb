62c6fbf28f9c4ca28124ce827391e9eb968d7f49
Mary Mederos <emau757@gmail.com>
2016-11-08 17:30:52 -0500

    Iteration 4 Sorted Hash Completed

--- Week4/Day16/EX_WordCentral/app/controllers/text_inspections_controller.rb
+++ Week4/Day16/EX_WordCentral/app/controllers/text_inspections_controller.rb
@@ -4,8 +4,21 @@ class TextInspectionsController < ApplicationController
 	end
 	def create
 		@text = params[:text_inspection][:user_text]
-		@word_count = @text.split(" ").length
+		input = @text.split(" ")
+		@word_count = input.length
 		@read_time = @word_count.to_f / 275
+		unsorted = {}
+		input.each do |word|
+			if unsorted[word] == nil 
+				unsorted[word] = 1
+			else
+				unsorted[word] += 1
+			end
+		end
+		@frequency = unsorted.sort_by { |word, count| -count }[0..9]
 		render "results"
+	end
+	def lipsum
+
 	end
 end
