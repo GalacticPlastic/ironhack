34feb42bc3e73b4dbf2845574dbe06f19e522a33
Mary Mederos <emau757@gmail.com>
2016-11-09 11:19:35 -0500

    Rails CRUD Actions Exercise Using Form Helpers

--- Week4/Day16/Lesson_Rails/timetracking/app/controllers/projects_controller.rb
+++ Week4/Day16/Lesson_Rails/timetracking/app/controllers/projects_controller.rb
@@ -3,4 +3,20 @@ class ProjectsController < ApplicationController
 		@projects = Project.order(created_at: :desc) .limit(10) #.where.not(descript: nil) <-- Removes No. 7 completely. No bueno?
 		render 'index'
 	end
+	def show
+		@my_project = Project.find(params[:id])
+		render 'show'
+	end
+	def new
+		@my_project = Project.new
+		render 'new'
+	end
+	def create
+		@my_project = Project.new(
+			:name => params[:project][:name],
+			:descript => params[:project][:descript]
+		)
+		@my_project.save
+		redirect_to project_prath(@my_project)
+	end
 end
--- Week4/Day16/Lesson_Rails/timetracking/config/routes.rb
+++ Week4/Day16/Lesson_Rails/timetracking/config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   get "/add_form", to: "calculator#add_form"
   post "/calculator", to: "calculator#process_addition"
   get "/projects", to: "projects#index"
+  get "/projects/new", to: "projects#new"
+  get "/projects/:id", to: "projects#show", as: "project"
+  post "/projects", to: "projects#create"
+  
+  # /New MUST be **ABOVE** /:ID!
+  # Link to create post is /projects, not /projects/create (naming convention).
+  # The reasoning behind the convention is that when you create something new, you want to add it to the list (index).
 end
