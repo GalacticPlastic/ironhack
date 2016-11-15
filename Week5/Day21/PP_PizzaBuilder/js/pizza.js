e3031176cae11b26e66eca022131b4a89064394e
Mary Mederos <emau757@gmail.com>
2016-11-15 09:34:17 -0500

    Refactored Event Handlers with Named Functions

--- Week5/Day21/PP_PizzaBuilder/js/pizza.js
+++ Week5/Day21/PP_PizzaBuilder/js/pizza.js
@@ -1,62 +1,64 @@
-console.log("CONSOLE LOG");
 $(document).ready(function () {
-	$('.btn-pepperonni').on('click', function () {
-		// console.log("Oh pepperoni, why so mad?");	<= Wiring Test
-		$('.pep').toggle();
-		$('.btn-pepperonni').toggleClass('active');
-		$('#price').empty();
-		if ($('.btn-pepperonni').hasClass('active')) {
-			totalPrice += pepPrice;
-			$('#pepperoni').toggle();
-		} else {
-			totalPrice -= pepPrice;
-            $('#pepperoni').toggle();
-		}
-		$('#price').append(totalPrice);
-		$('#price').prepend("$");
-	});
-	$('.btn-mushrooms').on('click', function () {
-		$('.mushroom').toggle();
-		$('.btn-mushrooms').toggleClass('active');
-		$('#price').empty();
-        if ($('.btn-mushrooms').hasClass('active')) {
-			totalPrice += mushPrice;
-   	        $('#mushroom').toggle();
-        } else {
-        	totalPrice -= mushPrice;
-   	        $('#mushroom').toggle();
-        }
-        $('#price').append(totalPrice);
-        $('#price').prepend("$");
-	});
-	$('.btn-green-peppers').on('click', function () {
-		$('.green-pepper').toggle();
-		$('.btn-green-peppers').toggleClass('active');
-		$('#price').empty();
-		if ($('.btn-green-peppers').hasClass('active')){
-			totalPrice += greenPrice;
-			$('#green-pepper').toggle();
-		} else {
-			totalPrice -= greenPrice;
-			$('#green-pepper').toggle();
-		}
-		$('#price').append(totalPrice);
-		$('#price').prepend("$");
-	});
+	$('.btn-pepperonni').on('click', changePepperoni);
+	$('.btn-mushrooms').on('click', changeShrooms);
+	$('.btn-green-peppers').on('click', changePeppers);
 	$('.btn-sauce').on('click', changeSauce);
 	$('.btn-crust').on('click', changeCrust);
 });
+function changePepperoni () {
+	$('.pep').fadeToggle();
+	$('.btn-pepperonni').toggleClass('active');
+	$('#price').empty();
+	if ($('.btn-pepperonni').hasClass('active')) {
+		totalPrice += pepPrice;
+		$('#pepperoni').toggle();
+	} else {
+		totalPrice -= pepPrice;
+		$('#pepperoni').toggle();
+	}
+	$('#price').append(totalPrice);
+	$('#price').prepend("$");
+}
+function changeShrooms () {
+	$('.mushroom').fadeToggle();
+	$('.btn-mushrooms').toggleClass('active');
+	$('#price').empty();
+	if ($('.btn-mushrooms').hasClass('active')) {
+		totalPrice += mushPrice;
+		$('#mushroom').toggle();
+	} else {
+		totalPrice -= mushPrice;
+		$('#mushroom').toggle();
+	}
+	$('#price').append(totalPrice);
+	$('#price').prepend("$");
+}
+function changePeppers () {
+	$('.green-pepper').fadeToggle();
+	$('.btn-green-peppers').toggleClass('active');
+	$('#price').empty();
+	if ($('.btn-green-peppers').hasClass('active')){
+		totalPrice += greenPrice;
+		$('#green-pepper').toggle();
+	} else {
+		totalPrice -= greenPrice;
+		$('#green-pepper').toggle();
+	}
+	$('#price').append(totalPrice);
+	$('#price').prepend("$");
+}
 function changeSauce () {
 	$('#price').empty();
+	$('#pizza-sauce').toggleClass('active');
 	if ($('#pizza-sauce').hasClass('sauce-white')) {
 		$('#pizza-sauce').removeClass('sauce-white');
 		$('.btn-sauce').text('White Sauce');
-		totalPrice -= crustPrice;
+		totalPrice -= saucePrice;
 		$('#white-sauce').toggle();
 	} else {
 		$('#pizza-sauce').addClass('sauce-white');
 		$('.btn-sauce').text('Regular Sauce');
-		totalPrice += crustPrice;
+		totalPrice += saucePrice;
 		$('#white-sauce').toggle();  
 	}
 	$('#price').append(totalPrice);
@@ -64,6 +66,7 @@ function changeSauce () {
 }
 function changeCrust () {
 	$('#price').empty();
+	$('#pizza-crust').toggleClass('active');
 	if ($('#pizza-crust').hasClass('crust-gluten-free')) {
 		$('#pizza-crust').removeClass('crust-gluten-free');
 		$('.btn-crust').text('Gluten-Free Crust');
