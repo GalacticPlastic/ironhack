575ac87facba2ce11c5ff90124adf3e7693d472b
Mary Mederos <emau757@gmail.com>
2016-11-15 06:53:11 -0500

    Revised jQuery Lesson Notes

--- Week5/Day21/Lesson_JQuery/README.md
+++ Week5/Day21/Lesson_JQuery/README.md
@@ -22,3 +22,49 @@ $(''.on(''), function () {
 - After		Outside, After
 
 <em>Note:</em> Cannot append or prepend content to self-closing tags.
+
+## Replacing the Dom Content
+- html()	Replaces Content with HTML (tags converted properly)
+- text()	Replaces Content with Plain Text
+
+## Removing Elements
+- remove()	Deletes Element
+- empty()	Deletes Content of Element
+
+
+```JavaScript
+$('p').remove();
+$('button').remove();
+```
+
+## Manipulating Properties
+
+```JavaScript
+$('input').prop('type');
+$('.container').prop('type');
+```
+
+## Manipulating Classes
+- .hasClass() method
+	=> Boolean check for a specific class
+- .addClass() method
+- .removeClass() method
+- .toggleClass() method
+
+## jQuery Effects
+1. Basics:
+
+	- .hide ()
+	- .show()
+	- .toggle()
+
+2. Opacity:
+
+	- .fadeIn()
+	- .fadeOut()
+	- .fadeToggle()
+
+3. Transitions:
+
+	- .slideIn()
+	- .slideOut()
--- Week5/Day21/Lesson_JQuery/js/app.js
+++ Week5/Day21/Lesson_JQuery/js/app.js
@@ -1,13 +1,15 @@
 console.log("CONSOLE LOG");
 $(document).ready(function () {
 	$('.js-pizza').on('click', function () {
-		$('.js-small').prepend(' PIZZA! PIZZA!');
+		// $('.js-small').text(' PIZZA! PIZZA!');
+		$('p').remove();
 	});
 	$('.empanadas').click(function () {
-		$('.js-heading').after(`
-			<span>Argentinian empanadas are the only good empanadas.</span>
-			<a href="#">Dame Uno!</a>
-		`)
+		// $('.js-heading').html(`
+		// 	<span>Argentinian empanadas are the only good empanadas.</span>
+		// 	<a href="#">Dame Uno!</a>
+		// `)
+		$('h1').empty();
 	});
 	$('#cookies').on('click', cookieMessage);
 	$(".js-button").on("click", function () {
@@ -24,17 +26,71 @@ $(document).ready(function () {
 	      $(".js-button").removeClass("btn-danger");
 	    }
 	});
+	$('.js-change-unicorn').on('click', changeUnicorn);
+
+	$('#masta-fade').on('click', fadeAll);
+
+	$('.unicorn-mode').on('click', function () {
+		$('*').addClass('.Unicornified');
+	});
 });
+function fadeAll () {
+	$(this).siblings().fadeOut();
+}
+// function fadeAll () {
+// 	if ($(this).hasClass('.masta-fade')) {
+// 		$(this).noop();
+// 	} else {
+// 		$(this).fadeOut();
+// 	}
+// }
+// function fadeAll () {
+// 	if ($('*'):not('.masta-fade')) {
+// 		$(this).fadeOut();
+// 	}
+// }
+
 function cookieMessage () {
-	var cookieContent = `
-		<br>
-		<section>
-			<img src="https://s-media-cache-ak0.pinimg.com/236x/03/70/e5/0370e5d7cf274f117ff1de0a0b65f484.jpg" />
-			<h3>PUT THAT COOKIE DOWN!</h3>
-			<h4>NOOOOWWW!!</h4>
-		</section>
-	`;
+	if ($('.cookie-popup').length > 0) {
+		$('.cookie-popup').remove();
+	} else {
+		var cookieContent = `
+			<section class="cookie-popup">
+				<img src="https://s-media-cache-ak0.pinimg.com/236x/03/70/e5/0370e5d7cf274f117ff1de0a0b65f484.jpg" />
+				// <h3>PUT THAT COOKIE DOWN!</h3>
+				// <h4>NOOOOWWW!!</h4>
+				<p>All I want to do is zoom-a-zoom-zoom-zoom</p>
+				<p>And a boom-boom</p>
+			</section>
+		`;
+	}
 	$('body').prepend(cookieContent);
+}
+function changeUnicorn () {
+	// console.log("Button Wiring Test");
+	// Always test wiring for basic syntax errors BEFORE getting into functionality
+	
+	unicornCount += 1;
+	if (unicornCount > 3) {
+		unicornCount = 1;
+	}
+	$('.js-unicorn2').attr("src", `images/unicorn${unicornCount}.gif`);
+}
+function changeBtnColor () {
+	$('button').addClass('btn');
+	// $('button').toggleClass('btn-success');
+	if ($('button').hasClass('btn-success')) {
+		$('button').removeClass('btn-success');
+		$('button').addClass('btn-info');
+	} else if ($('button').hasClass('btn-info')) {
+		$('button').removeClass('btn-info');
+		$('button').addClass('btn-warning');
+	} else if ($('button').hasClass('btn-warning')) {
+		$('button').removeClass('btn-warning');
+	} else {
+		$('button').addClass('btn-success');
+	}
 }
 var price = '<p>Only $399.99</p>';
 $('.container').append(price);
+var unicornCount = 1;
